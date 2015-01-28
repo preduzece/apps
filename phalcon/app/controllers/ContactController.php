3225e9f49fa086bd47b725a03c8a50d97131fb52
MIloš Dodić <milos_dodic@live.com>
2015-01-28 10:27:21 +0100

    pogled 2 out

--- phalcon/app/controllers/ContactController.php
+++ phalcon/app/controllers/ContactController.php
@@ -8,4 +8,31 @@ class ContactController extends \Phalcon\Mvc\Controller
         $this->view->pick("front/contact");
     }
 
+    public function registerAction()
+    {
+    	$contact = new Message();
+
+        //Store and check for errors
+        $success = $contact->save(
+        	$this->request->getPost(), array(
+        		'name', 'email', 'gender', 'text'));
+
+        if ($success) {
+
+    		$this->view->setVar("status", "Message sent!");
+            $this->view->pick("front/contact");
+        } else {
+
+        	$status = '';
+
+        	foreach ($contact->getMessages() as $error) {
+				$status .= $error->getMessage(). "<br/>";
+			}
+
+    		$this->view->setVar("status", $status);
+            $this->view->pick("front/contact");
+
+        }
+    }
+
 }
