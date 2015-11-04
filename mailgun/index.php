7a88a0c125091eb141994c31edbb0814fb7ae6d0
Milos Dodic <dmilos91@gmail.com>
2015-11-04 01:03:07 +0100

    mailgun small fixes

--- mailgun/index.php
+++ mailgun/index.php
@@ -5,11 +5,14 @@ use Mailgun\Mailgun;
 
 # First, instantiate the SDK with your API credentials and define your domain.
 $mailGun = new Mailgun("key-d614c175cd653e6144c227224ceb685e");
+$message = 'It is so simple to send a message.';
 $domain = "gdezavikend.rs";
 
 # Now, compose and send your message.
 $mailGun->sendMessage($domain, array('from'    => 'office@gdezavikend.rs',
                                 'to'      => 'milos_dodic@live.com',
                                 'subject' => 'The PHP SDK is awesome!',
-                                'text'    => 'It is so simple to send a message.'));
+                                'text'    => $message));
+
+var_dump($mailGun); die();
 ?>
