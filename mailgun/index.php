ac0391b86fb4eba2e516fc53709137a6d82a014a
Milos Dodic <dmilos91@gmail.com>
2015-11-04 22:20:38 +0100

    mailgun html template

--- mailgun/index.php
+++ mailgun/index.php
@@ -3,17 +3,32 @@
 require 'vendor/autoload.php';
 use Mailgun\Mailgun;
 
+function render($path, $message){
+    ob_start();
+
+    include $path;
+    $content = ob_get_contents();
+
+    ob_end_clean();
+    return $content;
+}
+
 # First, instantiate the SDK with your API credentials and define your domain.
 $mailGun = new Mailgun("key-d614c175cd653e6144c227224ceb685e");
 $message = 'It is so simple to send a message.';
 $domain = "gdezavikend.rs";
 
+$htmlMessage = render('mail.php', $message);
+
+// var_dump($htmlMessage); die;
+
 # Now, compose and send your message.
 $mailGun->sendMessage($domain, [
     'from'    => 'office@gdezavikend.rs',
-    'to'      => 'milos_dodic@live.com',
+    'to'      => 'dmilos91@gmail.com',
     'subject' => 'The PHP SDK is awesome!',
-    'text'    => $message
+    'html'    => (string) $htmlMessage,
+    // 'text'    => (string) $message
     ]
 );
 
---
+++ mailgun/mail.php
@@ -0,0 +1,5 @@
+<div>
+    <h3>Hello, </h3>
+    <p><?= $message ?></p><hr/>
+    <b>MailGun</b>
+</div>
