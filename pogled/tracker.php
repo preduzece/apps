beacfb8082ad7b5b12554709e6af9b07c43821b2
Milos Dodic <dmilos91@gmail.com>
2015-11-05 20:55:20 +0100

    small fixes

--- pogled/tracker.php
+++ pogled/tracker.php
@@ -25,8 +25,9 @@
         # Now, compose and send your message.
         $mailGun->sendMessage($domain, [
             'from'    => 'tracking@pogled.co.rs',
-            'to'      => 'office@pogled.co.rs',
-            // 'to'      => 'dmilos91@gmail.com',
+            'to'      => 'pogled.rs@gmail.com',
+            // 'to'      => 'office@pogled.co.rs',
+            // 'to'      => 'milos_dodic@live.com',
             'subject' => 'Pracenje tovara',
             'html'    => $content,
             ]
