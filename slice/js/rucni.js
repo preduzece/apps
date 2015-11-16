baa6e90e303e4846aa9bdc50db3827eab586d9e1
stefanveljkovicvr <stefanveljkovicvr@gmail.com>
2015-11-16 22:32:41 +0100

    novi slice

--- slice/js/rucni.js
+++ slice/js/rucni.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(document).ready(function() {
 			$("#splash").zAccordion({
 				timeout: 4500,
@@ -19,26 +18,4 @@ $(document).ready(function() {
 				width: 900,
 				height: 310
 			});
-=======
-$(document).ready(function() {
-			$("#splash").zAccordion({
-				timeout: 4500,
-				speed: 500,
-				slideClass: 'slide',
-				animationStart: function () {
-					$('#splash').find('li.slide-previous div').fadeOut();
-				},
-				animationComplete: function () {
-					$('#splash').find('li.slide-open div').fadeIn();
-				},
-				buildComplete: function () {
-					$('#splash').find('li.slide-closed div').css('display', 'none');
-					$('#splash').find('li.slide-open div').fadeIn();
-				},
-				startingSlide: 1,
-				slideWidth: 600,
-				width: 900,
-				height: 310
-			});
->>>>>>> 9a9663c19ba89cd549fa1dda8532108174447669
 		});
--- slice/slider.php
+++ slice/slider.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <div class="recommended_items">
     <h2 class="title text-center">gde za vikend top ponuda</h2>
     <div id="recommended-item-carousel" class="carousel slide" data-ride="carousel">
@@ -33,40 +32,4 @@
             </li>
         </ul>
     </div>
-=======
-<div class="recommended_items">
-    <h2 class="title text-center">gde za vikend top ponuda</h2>
-    <div id="recommended-item-carousel" class="carousel slide" data-ride="carousel">
-        <ul id="splash">
-            <li>
-                <img src="images/1.jpg" alt="" />
-                <div>
-                    <strong>Old School Diner</strong>
-                    <p class="splash-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras porttitor lacus sollicitudin ligula sagittis a ultricies nulla ultricies. Ut odio nisi, posuere sed blandit at, bibendum non dolor.</p>
-                </div>
-            </li>
-            <li>
-                <img src="images/2.jpg" alt="" />
-                <div>
-                    <strong>A Day at the Pool</strong>
-                    <p class="splash-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras in condimentum sem. Aenean faucibus dignissim auctor. In ut libero vitae augue laoreet iaculis at a tellus.</p>
-                </div>
-            </li>
-            <li>
-                <img src="images/3.jpg" alt="" />
-                <div>
-                    <strong>Fill it Up!</strong>
-                    <p class="splash-text">Duis viverra velit orci. Sed vestibulum mi nec est imperdiet sed ullamcorper augue molestie. Donec ultrices facilisis erat at porttitor.</p>
-                </div>
-            </li>
-            <li>
-                <img src="images/5.jpg" alt="" />
-                <div>
-                    <strong>Going for a Drive</strong>
-                    <p class="splash-text">Phasellus sed lectus nisl, eget cursus eros. Suspendisse posuere orci eu lorem luctus et porta nunc posuere. Cras sed lectus vitae leo accumsan adipiscing.</p>
-                </div>
-            </li>
-        </ul>
-    </div>
->>>>>>> 9a9663c19ba89cd549fa1dda8532108174447669
 </div><!--/recommended_items-->
