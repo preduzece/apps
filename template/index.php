a17971ff827274ecdfcabfa72a478fa5b6f8db7e
d0dic <milos.dodic@degordian.com>
2016-01-31 19:54:58 +0100

    reorder

---
+++ template/index.php
@@ -0,0 +1,172 @@
+<?php
+$title = "Home";
+
+$content = '<!--recommended_items--><br>
+<div class="recommended_items">
+    <h2 class="title text-center">gde za vikend - top ponuda</h2>
+    
+    <div id="recommended-item-carousel" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="item1">   
+                <div class="product-image-wrapper fixer">
+                    <div class="single-products">
+                        <div class="productinfo text-center">
+                            <a href="" title="" target="_blank">
+                                <img src="images/1.jpg" class="img-responsive" />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <a class="left recommended-item-control" href="#recommended-item-carousel" data-slide="prev">
+            <i class="fa fa-angle-left"></i>
+        </a>
+        <a class="right recommended-item-control" href="#recommended-item-carousel" data-slide="next">
+            <i class="fa fa-angle-right"></i>
+        </a>            
+    </div>
+</div><!--/recommended_items-->
+
+<div class="features_items"><!--features_items-->
+    <h2 class="title text-center">Top 3 preporuka gde za vikend</h2>
+    <div class="col-sm-4">
+        <div class="product-image-wrapper" >
+            <div class="single-products">
+                <div class="productinfo text-center">
+                    <img src="images/offers/1.jpg" alt="" />
+                    <h2>Teretana - Gym</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nm sapiente ipsa natus nisi.</p>
+                    <a href="#" class="btn btn-default add-to-cart">Vidi detalje</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-4" >
+        <div class="product-image-wrapper" >
+            <div class="single-products">
+                <div class="productinfo text-center">
+                    <img src="images/offers/5.jpg" alt="" />
+                    <h2>Teretana - Gym</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nm sapiente ipsa natus nisi.</p>
+                    <a href="#" class="btn btn-default add-to-cart">Vidi detalje</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-4">
+        <div class="product-image-wrapper" >
+            <div class="single-products">
+                <div class="productinfo text-center">
+                    <img src="images/offers/6.jpg" alt="" />
+                    <h2>Teretana - Gym</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nm sapiente ipsa natus nisi.</p>
+                    <a href="#" class="btn btn-default add-to-cart">Vidi detalje</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="features_items"><!--features_items-->
+    <h2 class="title text-center">gde za vikend preporuka</h2>
+    <div class="col-sm-3">
+        <div class="product-image-wrapper">
+            <div class="single-products">
+                <div class="productinfo text-center">
+                    <img src="images/offers/2.jpg" alt="" />
+                    <h2>Teretana - Gym</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nm sapiente ipsa natus nisi.</p>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-3">
+        <div class="product-image-wrapper">
+            <div class="single-products">
+                <div class="productinfo text-center">
+                    <img src="images/offers/2.jpg" alt="" />
+                    <h2>Teretana - Gym</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nm sapiente ipsa natus nisi.</p>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-3">
+        <div class="product-image-wrapper">
+            <div class="single-products">
+                <div class="productinfo text-center">
+                    <img src="images/offers/2.jpg" alt="" />
+                    <h2>Teretana - Gym</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nm sapiente ipsa natus nisi.</p>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-3">
+        <div class="product-image-wrapper">
+            <div class="single-products">
+                <div class="productinfo text-center">
+                    <img src="images/offers/2.jpg" alt="" />
+                    <h2>Teretana - Gym</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nm sapiente ipsa natus nisi.</p>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-3">
+        <div class="product-image-wrapper">
+            <div class="single-products">
+                <div class="productinfo text-center">
+                    <img src="images/offers/3.jpg" alt="" />
+                    <h2>Pucanje</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nm sapiente ipsa natus nisi.</p>
+                    <a href="#" class="btn btn-default add-to-cart">Vidi detalje</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-3">
+        <div class="product-image-wrapper">
+            <div class="single-products">
+                <div class="productinfo text-center">
+                    <img src="images/offers/4.jpg" alt="" />
+                    <h2>Kafic - kafeterija</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nm sapiente ipsa natus nisi.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-3">
+        <div class="product-image-wrapper">
+            <div class="single-products">
+                <div class="productinfo text-center">
+                    <img src="images/offers/2.jpg" alt="" />
+                    <h2>Teretana - Gym</h2>
+                    <p >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nm sapiente ipsa natus nisi.</p>
+                    <a href=""> <p class="text-right">Detaljnije>></p></a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-3" >
+        <div class="product-image-wrapper" style="box-shadow: 5px 5px 2px silver;">
+            <div class="single-products">
+                <div class="productinfo text-center">
+                    <img src="images/offers/3.jpg" alt="" />
+                    <h2>Pucanje</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nm sapiente ipsa natus nisi.</p>
+                    <a href="#" class="btn btn-default add-to-cart">Vidi detalje</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div><!--features_items-->
+';
+include 'Template.php';
+?>
